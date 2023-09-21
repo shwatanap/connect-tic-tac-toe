5b3ebdef1ebc8d560ea474300cfb128f411866c4
shwatanap <watanabeshou1520@gmail.com>
2023-09-21 18:59:43 +0900

    :fire: コメント削除

--- src/connect/cmd/server/main.go
+++ src/connect/cmd/server/main.go
@@ -12,33 +12,9 @@ import (
 )
 
 func main() {
-	// port := 50051
-	// lis, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
-	// if err != nil {
-	// 	log.Fatalf("failed to listen: %v", err)
-	// }
-
-	// server := grpc.NewServer()
-
-	// pb.RegisterMatchingServiceServer(server, handler.NewMatchingHandler())
-	// pb.RegisterGameServiceServer(server, handler.NewGameHandler(gu))
 	mux := http.NewServeMux()
 	mux.Handle(gamev1connect.NewGameServiceHandler(handler.NewGameHandler()))
 	mux.Handle(matchingv1connect.NewMatchingServiceHandler(handler.NewMatchingHandler()))
 
 	http.ListenAndServe(":50051", h2c.NewHandler(mux, &http2.Server{}))
-	// http.ListenAndServe(":50051", mux)
-
-	// reflection.Register(server)
-
-	// go func() {
-	// 	log.Printf("start gRPC server port: %v", port)
-	// 	server.Serve(lis)
-	// }()
-
-	// quit := make(chan os.Signal)
-	// signal.Notify(quit, os.Interrupt)
-	// <-quit
-	// log.Println("stopping gRPC server...")
-	// server.GracefulStop()
 }
